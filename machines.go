90fc431799085326e161f59ca53a01da1d49b094
Frederic Gingras <frederic@gingras.cc>
2015-04-22 18:00:48 -0400

    Add the GetMachine and CreateMachine methods

--- machines.go
+++ machines.go
@@ -6,8 +6,9 @@ import (
 
 // Machine represents an SDC instance.
 type Machine struct {
-	Id      string `json:"id"`
-	Name    string `json:"name"`
+	Id   string `json:"id"`
+	Name string `json:"name"`
+	// Can either be "virtual" or "smart"
 	Type    string `json:"type"`
 	State   string `json:"state"`
 	Dataset string `json:"dataset"`
@@ -18,7 +19,9 @@ type Machine struct {
 	Ips      []string          `json:"ips"`
 	Metadata map[string]string `json:"metadata"`
 
+	// Formated in ISO 8601
 	Created time.Time `json:"created"`
+	// Formated in ISO 8601
 	Updated time.Time `json:"updated"`
 
 	Package     string `json:"package"`
@@ -26,8 +29,43 @@ type Machine struct {
 	Credentials bool   `json:"credentials"`
 }
 
+// ListMachines fetches all your machines from the SDC API.
 func (c *Client) ListMachines() ([]*Machine, error) {
 	response := []*Machine{}
 	_, err := c.Get("/my/machines", &response)
+	return response, err
+}
+
+// GetMachine fetches a specific machine from the SDC api.
+func (c *Client) GetMachine(id string) (*Machine, error) {
+	response := *Machine{}
+	_, err := c.Get("/my/machines/"+id, &response)
+	return response, err
+}
+
+// CreateMachineRequest represents the parameter needed for calling CreateMachine.
+type CreateMachineRequest struct {
+	Name    string `json:"name"`
+	Image   string `json:"image"`
+	Package string `json:"package"`
+
+	Networks        []string `json:"networks"`
+	DefaultNetworks []string `json:"default_networks"`
+
+	Metadata map[string]string `json:"metadata"`
+	Tags     map[string]string `json:"tags"`
+
+	FirewallEnabled bool `json:"firewall_enabled"`
+}
+
+// CreateMachine creates a new machine in SDC. The only two required parameters
+// are `image` and `package`.
+//
+// The returned machine will be incomplete and you will need to
+// poll the GetMachine method utils the `state` is equal to "running"
+// for logging in and getting ips.
+func (c *Client) CreateMachine(request CreateMachineRequest) (*Machine, error) {
+	response := *Machine{}
+	_, err := c.Post("/my/machines", &request, &response)
 	return response, err
 }
